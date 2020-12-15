57d17623ad05aeee66652ef6807872a995c5cac7
w84scaler <saint.neki@gmail.com>
2020-12-15 23:49:18 +0300

    console done

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
@@ -10,8 +8,41 @@ namespace lab4
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
+            TestsGenerator generator = new TestsGenerator();
+            ExecutionDataflowBlockOptions blockOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 3 };
+            TransformBlock<string, string> getCode = new TransformBlock<string, string>
+            (
+                async filePath => await File.ReadAllTextAsync(filePath),
+                blockOptions
+            );
+
+            TransformManyBlock<string, TestFile> createTests = new TransformManyBlock<string, TestFile>
+            (
+                async sourceCode => await Task.Run(() => generator.CreateTests(sourceCode).ToArray()),
+                blockOptions
+            );
+
+            ActionBlock<TestFile> saveTests = new ActionBlock<TestFile>
+            (
+                async testsFile => await File.WriteAllTextAsync(Directory.GetCurrentDirectory() + "\\Tests\\" + testsFile.Name, testsFile.Code),
+                blockOptions
+            );
+
+            DataflowLinkOptions linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+            getCode.LinkTo(createTests, linkOptions);
+            createTests.LinkTo(saveTests, linkOptions);
+
+            string[] filePaths = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Files\\");
+
+            foreach (string filePath in filePaths)
+            {
+                if (filePath.EndsWith(".cs"))
+                    getCode.Post(filePath);
+            }
+            getCode.Complete();
+            saveTests.Completion.Wait();
         }
     }
 }
