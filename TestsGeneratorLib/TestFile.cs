641ded939ff89cc334aba8965d74ba4d9ae51ed2
w84scaler <saint.neki@gmail.com>
2020-12-15 23:03:20 +0300

    created all projects; lib done

---
+++ TestsGeneratorLib/TestFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestsGeneratorLib
+{
+    public class TestFile
+    {
+        public string Name { get; }
+        public string Code { get; }
+
+        internal TestFile(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+using TestsGeneratorLib;
+
+namespace lab4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+        }
+    }
+}
